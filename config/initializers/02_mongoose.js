8d0a56f6d83f273c4347b545e1b96333eaaeeb6a
Aleksandar Dezelin <dezelin@gmail.com>
2013-01-18 00:54:53 +0100

    Fixed MongoDB options parsing.
    Exception is being thrown constantly:
    "Error: port must be specified"

--- config/initializers/02_mongoose.js
+++ config/initializers/02_mongoose.js
@@ -1,5 +1,42 @@
-var mongoose = require('mongoose')
+var assert = require('assert')
+  , mongoose = require('mongoose')
   , mongooseTypes = require('mongoose-types');
+
+DEFAULT_MONGOOSE_SERVER = 'localhost';
+DEFAULT_MONGOOSE_SERVER_PORT = '27017';
+DEFAULT_MONGOOSE_DB = 'talk_io';
+
+function parseUri(options) {
+  assert(options, 'Illegal argument.');
+
+  var hosts = options.hostnames || [DEFAULT_MONGOOSE_SERVER + 
+    ':' + DEFAULT_MONGOOSE_SERVER_PORT];
+  var db_name = options.database || DEFAULT_DB_NAME;
+  var user = options.username || '';
+  var passw = options.password || '';
+
+  var uri = '';
+  hosts.forEach(function (val, index, array) {
+    assert('string' == typeof val, 'Illegal type.');
+
+    var db_uri = 'mongodb://';
+    if (user) {
+      db_uri += user;
+      if (passw)
+        db_uri += ':\'' + passw + '\'';
+
+      db_uri += '@';
+    }
+
+    db_uri += val + '/' + db_name;
+    if (index < array.length - 1)
+      db_uri += ',';
+
+    uri += db_uri;
+  });
+
+  return uri;
+}
 
 module.exports = function () {
   // Any files in this directory will be `require()`'ed when the application
@@ -12,13 +49,12 @@ module.exports = function () {
   // finished.  Initializers are invoked sequentially, ensuring that the
   // previous one has completed before the next one executes.
 
-  // Open default db connection
-  var options = this.config.mongooseOptions;
-  var host = options.hostname;
-  var port = options.port;
-  var db_name = options.db;
+  var self = this;
 
-  this.db = mongoose.connect(host, db_name, port, {});
+  // Open default db connection
+  var options = self.config.mongooseOptions;
+  var db_uri = parseUri(options);
+  self.db = mongoose.connect(db_uri, options);
 
   // Load additional mongoose types
   mongooseTypes.loadTypes(mongoose);
