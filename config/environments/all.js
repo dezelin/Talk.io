fc501f449b563ea89ff70f93d9b49f549d8e58e1
Aleksandar Dezelin <dezelin@gmail.com>
2013-01-18 09:56:54 +0100

    connect-mongodb removed from dependencies.

--- config/environments/all.js
+++ config/environments/all.js
@@ -3,60 +3,63 @@ var express = require('express')
   , passport = require('passport')
   , mongoose = require('mongoose')
   , mongooseDataStore = require('locomotive-mongoose')
-  , mongoStore = require('connect-mongodb')
   , util = require('util');
 
+
 module.exports = function () {
+  var self = this;
   // Warn of version mismatch between global "lcm" binary and local installation
   // of Locomotive.
-  if (this.version !== require('locomotive').version) {
-    console.warn(util.format('version mismatch between local (%s) and global (%s) Locomotive module', require('locomotive').version, this.version));
+  if (self.version !== require('locomotive').version) {
+    console.warn(util.format('version mismatch between local (%s) and global (%s) Locomotive module', require('locomotive').version, self.version));
   }
 
   // Configure application settings.  Consult the Express API Reference for a
   // list of the available [settings](http://expressjs.com/api.html#app-settings).
-  this.set('views', __dirname + '/../../app/views');
-  //this.set('view engine', 'ejs');
-  this.set('view engine', 'jade');
+  self.set('views', __dirname + '/../../app/views');
+  //self.set('view engine', 'ejs');
+  self.set('view engine', 'jade');
+  self.set('view options', {
+    layout: false
+  });
 
   // Register EJS as a template engine.
-  //this.engine('ejs', require('ejs').__express);
-  this.engine('jade', require('jade').__express);
+  //self.engine('ejs', require('ejs').__express);
+  self.engine('jade', require('jade').__express);
 
   // Override default template extension.  By default, Locomotive finds
   // templates using the `name.format.engine` convention, for example
   // `index.html.ejs`  For some template engines, such as Jade, that find
-  // layouts using a `layout.engine` notation, this results in mixed conventions
-  // that can cuase confusion.  If this occurs, you can map an explicit
+  // layouts using a `layout.engine` notation, self results in mixed conventions
+  // that can cuase confusion.  If self occurs, you can map an explicit
   // extension to a format.
-  this.format('html', { extension: '.jade' });
+  self.format('html', { extension: '.jade' });
 
   // Register formats for content negotiation.  Using content negotiation,
   // different formats can be served as needed by different clients.  For
   // example, a browser is sent an HTML response, while an API client is sent a
   // JSON or XML response.
-  /* this.format('xml', { engine: 'xmlb' }); */
+  /* self.format('xml', { engine: 'xmlb' }); */
 
   // Load config file.
-  this.config = require('../config.json');
+  self.config = require('../config.json');
 
   // Use middleware.  Standard [Connect](http://www.senchalabs.org/connect/)
   // middleware is built-in, with additional [third-party](https://github.com/senchalabs/connect/wiki)
   // middleware available as separate modules.
-  this.use(poweredBy('Locomotive'));
-  this.use(express.logger());
-  this.use(express.favicon());
-  this.use(express.static(__dirname + '/../../public'));
-  this.use(express.bodyParser());
-  this.use(express.methodOverride());
-  this.use(express.cookieParser());
-  this.use(express.session({
-    secret: this.config.sessionSecret,
-    store: new mongoStore({ db: mongoose.connection.db })
+  self.use(poweredBy('Locomotive'));
+  self.use(express.logger());
+  self.use(express.favicon());
+  self.use(express.static(__dirname + '/../../public'));
+  self.use(express.bodyParser());
+  self.use(express.methodOverride());
+  self.use(express.cookieParser());
+  self.use(express.session({
+    secret: self.config.sessionSecret
   }));
-  this.use(passport.initialize());
-  this.use(passport.session());
-  this.use(this.router);
+  self.use(passport.initialize());
+  self.use(passport.session());
+  self.use(self.router);
 
-  this.datastore(mongooseDataStore);
+  self.datastore(mongooseDataStore);
 }
