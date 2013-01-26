b9f0f0e6fd45102739cab3b3d7afbaaabeb422b2
Aleksandar Dezelin <dezelin@gmail.com>
2013-01-26 23:35:09 +0100

    Added logging.
    Logging has been added for factory instance creation.

--- app/common/config/stack_info_factory.js
+++ app/common/config/stack_info_factory.js
@@ -11,6 +11,8 @@ var StackInfoFactory = (function () {
 		var bucket = StackType.getStackType();
 		var typeName = type.toString();
 		var StackInfo = types[bucket + typeName];
+		logger.info('Creating stack info instance of the type \'' + typeName +
+			'\' from bucket \'' + bucket + '\'');
 		return (StackInfo ? new StackInfo() : null);
 	}
 
@@ -21,7 +23,7 @@ var StackInfoFactory = (function () {
 		var bucket = stackType.toString();
 		var typeName = type.toString();
 		types[bucket + typeName] = StackInfo;
-		logger.info('Registered stack info type \'' + typeName 
+		logger.info('Registered stack info type \'' + typeName
 			+ '\' into bucket \'' + bucket + '\'.');
 		return StackInfoFactory;
 	}
